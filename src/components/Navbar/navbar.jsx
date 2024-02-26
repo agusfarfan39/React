61fde26a12ca76035421986cd8dfe3f3209d8ed5
tu-usuario <agustinafarfan7@hotmail.com>
2024-02-26 19:23:20 -0300

    Primer pre-entrega

---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+
+        <nav><h1>Hockey Planet</h1>
+
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Envios</li>
+                <li>Contacto</li>
+
+            </ul>
+
+
+        </nav>
+
+
+    )
+}
+
+export default navbar
