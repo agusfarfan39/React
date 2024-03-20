c28734f52aff2936f8d3ee65513bae00788cf684
tu-usuario <agustinafarfan7@hotmail.com>
2024-03-20 19:26:04 -0300

    segunda pre-entrega

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import './App.css'
 import Navbar from './components/Navbar/navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+    
+    <BrowserRouter>
     <Navbar />
-    <ItemListContainer greeting={'Bienvenidos a Hockey Planet'}/>
+    <Routes>
+      <Route path='/' element={<ItemListContainer greeting={'Bienvenidos a Hockey Planet'}/>}/>
+      <Route path='/item/:itemId' element={<ItemDetailContainer />}/>
+    
     <ItemDetailContainer/>
+    </Routes>
+   
+    </BrowserRouter>
+
+    
     </>
   )
 }
